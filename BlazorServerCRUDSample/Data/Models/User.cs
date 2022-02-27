a9137e7635118c1939176b8ae9489fb747a951fd
yoshi1220 <19341602+yoshi1220@users.noreply.github.com>
2022-02-27 22:45:26 +0900

    Userモデルの修正

--- BlazorServerCRUDSample/Data/Models/User.cs
+++ BlazorServerCRUDSample/Data/Models/User.cs
@@ -14,11 +14,13 @@ namespace BlazorServerCRUDSample.Data.Models
         [Key]
         public int UserId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "ユーザー名は必須です")]
         public string UserName { get; set; }
 
+        [Required(ErrorMessage = "Mailアドレスは必須です")]
         public string MailAddress { get; set; }
 
+        [Required(ErrorMessage = "誕生日は必須です")]
         public DateTime BirthDay { get; set; }
     }
 }
