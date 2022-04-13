b7a07ee6e1ef1f32cd2916dd9c3599d4671ee2a2
yoshi1220 <19341602+yoshi1220@users.noreply.github.com>
2022-04-13 13:52:32 +0900

    Loggerは標準のものに変更

--- BlazorServerCRUDSample/Program.cs
+++ BlazorServerCRUDSample/Program.cs
@@ -1,6 +1,4 @@
-﻿using BlazorServerCRUDSample;
-using BlazorServerCRUDSample.Data;
-using BlazorServerCRUDSample.Logger;
+﻿using BlazorServerCRUDSample.Data;
 using BlazorServerCRUDSample.Repositories;
 using BlazorServerCRUDSample.Services;
 using Microsoft.EntityFrameworkCore;
@@ -24,9 +22,6 @@ builder.Services.AddScoped<IUserRepository, UserRepository>(); //EntityFramework
 
 //Service関連            
 builder.Services.AddScoped<IUserService, UserService>();
-
-//Logger関連
-builder.Services.AddSingleton<ILogger, ConsoleLogger>(); //実際には必要なLoggerを実装してください。
 
 //MudBlazor
 builder.Services.AddMudServices();
